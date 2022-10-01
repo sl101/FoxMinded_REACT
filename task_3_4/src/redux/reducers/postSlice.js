1d6069114c329d8918cd3c1a67f94c89cab9f550
V Zhevaha <v.zhevaga@gmail.com>
2022-10-01 19:19:22 +0200

    create FormPost component, implemented adding message using API

--- task_3_4/src/redux/reducers/postSlice.js
+++ task_3_4/src/redux/reducers/postSlice.js
@@ -10,11 +10,16 @@ const postSlice = createSlice({
 
   reducers: {
     receivePosts(state, action) {
-      state.posts = action.payload;
+      state.posts = action.payload.reverse();
+    },
+    inputPost(state, action) {
+      const { body, title, userId } = action.payload;
+      const id = state.posts.length + 1;
+      state.posts.unshift({ body, title, userId, id });
     }
   }
 });
 
-export const { receivePosts } = postSlice.actions;
+export const { receivePosts, inputPost } = postSlice.actions;
 
 export default postSlice.reducer;
