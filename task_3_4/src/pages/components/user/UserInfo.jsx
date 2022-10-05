447a84cd2129fa467b4a7614ee9d80f62f2962dd
V Zhevaha <v.zhevaga@gmail.com>
2022-10-05 20:44:17 +0200

    included fontawesome icons

--- task_3_4/src/pages/components/user/UserInfo.jsx
+++ task_3_4/src/pages/components/user/UserInfo.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+  faEnvelope,
+  faPhone,
+  faHouse,
+  faGlobe,
+  faIndustry
+} from '@fortawesome/free-solid-svg-icons';
 import style from './UserInfo.module.scss';
 
 const User = ({ user: { name, email, phone, website, address, company } }) => {
@@ -14,12 +23,35 @@ const User = ({ user: { name, email, phone, website, address, company } }) => {
     <section className={style.userInfo}>
       <div className={style.inner}>
         <ul className={style.list}>
-          <li className={style.item}>{name}</li>
-          <li className={style.item}>{userEmail}</li>
-          <li className={style.item}>{userPhone}</li>
-          <li className={style.item}>{userAddress}</li>
-          <li className={style.item}>{website}</li>
-          <li className={style.item}>{userCompany}</li>
+          <li className={style.item}>
+            <FontAwesomeIcon className={style.icon} icon={faUser} />
+            <span className={style.value}>{name}</span>
+          </li>
+
+          <li className={style.item}>
+            <FontAwesomeIcon className={style.icon} icon={faEnvelope} />
+            <span className={style.value}>{userEmail}</span>
+          </li>
+
+          <li className={style.item}>
+            <FontAwesomeIcon className={style.icon} icon={faPhone} />
+            <span className={style.value}>{userPhone}</span>
+          </li>
+
+          <li className={style.item}>
+            <FontAwesomeIcon className={style.icon} icon={faHouse} />
+            <span className={style.value}>{userAddress}</span>
+          </li>
+
+          <li className={style.item}>
+            <FontAwesomeIcon className={style.icon} icon={faGlobe} />
+            <span className={style.value}>{website}</span>
+          </li>
+
+          <li className={style.item}>
+            <FontAwesomeIcon className={style.icon} icon={faIndustry} />
+            <span className={style.value}>{userCompany}</span>
+          </li>
         </ul>
       </div>
     </section>
