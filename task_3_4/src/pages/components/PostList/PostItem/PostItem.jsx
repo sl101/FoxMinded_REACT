b309bf54d104b8396e6d63f7035efcfc899f7eda
V Zhevaha <v.zhevaga@gmail.com>
2022-10-06 21:10:01 +0200

    fixed destructurisation

--- task_3_4/src/pages/components/PostList/PostItem/PostItem.jsx
+++ task_3_4/src/pages/components/PostList/PostItem/PostItem.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import style from './PostItem.module.scss';
 
-export const PostItem = (props) => {
-  const { title, body } = props.post;
-  return (
-    <li className={style.item}>
-      <h3 className={style.title}>{title}</h3>
-      <span className={style.body}>{body}</span>
-    </li>
-  );
-};
+export const PostItem = ({ post: { title, body } }) => (
+  <li className={style.item}>
+    <h3 className={style.title}>{title}</h3>
+    <span className={style.body}>{body}</span>
+  </li>
+);
--- task_3_4/src/pages/components/UserList/UserItem/UserItem.jsx
+++ task_3_4/src/pages/components/UserList/UserItem/UserItem.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import style from './UserItem.module.scss';
 
-export const UserItem = (props) => {
-  const { id, name, username } = props.user;
-
-  return (
-    <li className={style.item}>
-      <NavLink className={style.name} to={'/userpage/' + id}>
-        {name}
-      </NavLink>
-      <span className={style.username}>{username}</span>
-    </li>
-  );
-};
+export const UserItem = ({ user: { id, name, username } }) => (
+  <li className={style.item}>
+    <NavLink className={style.name} to={'/userpage/' + id}>
+      {name}
+    </NavLink>
+    <span className={style.username}>{username}</span>
+  </li>
+);
